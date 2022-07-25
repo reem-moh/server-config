b3875a1d202620543b996a6a55c150e8a52a0cf7
reemalgabani <ralgabani@channels.com.sa>
2022-07-25 11:13:14 +0300

    server config project

---
+++ ServerConfig/src/main/kotlin/com/ServerConfig/ServerConfig/ServerConfigApplication.kt
@@ -0,0 +1,11 @@
+package com.ServerConfig.ServerConfig
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerConfigApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerConfigApplication>(*args)
+}
